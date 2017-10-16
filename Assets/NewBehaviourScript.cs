99204dc62e77368a850e77756c01db65be020dda
ShrinikethNarayana <shrinikethNarayana@gmail.com>
2017-10-16 16:48:54 -0700

    Added a new Scene
    Scene has sprite with sideways movement

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector2.left * .25);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector2.right * .25);
+    }
+}
