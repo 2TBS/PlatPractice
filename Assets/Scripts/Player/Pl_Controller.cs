c0954c273472b8c1998db622a617426840f5858e
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-10-18 19:05:21 -0700

    Simple movement - Horizontal

---
+++ Assets/Scripts/Player/Pl_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pl_Controller : MonoBehaviour {
+	public Rigidbody2D PlayerRG;
+
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//simple Horizontal movement
+		 float value = Input.GetAxis ("Horizontal"); 
+		 PlayerRG.AddForce(new Vector2(value,0)*speed);
+	}
+}
