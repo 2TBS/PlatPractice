b4401d1b1c2e9ec30420757abdfa2a57191ee527
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-10-18 20:39:49 -0700

    Raycast Jump

--- Assets/Scripts/Player/Pl_Controller.cs
+++ Assets/Scripts/Player/Pl_Controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Pl_Controller : MonoBehaviour {
 	public Rigidbody2D PlayerRG;
-
+	public LayerMask layerM;
 	public int speed;
 	public int jumpForce;
 	// Use this for initialization
@@ -14,10 +14,24 @@ public class Pl_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		//Horizontal Movement
 		float value = Input.GetAxis ("Horizontal"); 
 		PlayerRG.velocity = new Vector2(value*speed,PlayerRG.velocity.y);
+		//Jump movement
 		if(Input.GetButtonDown("Jump")){
-			PlayerRG.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+			//Raycast
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.0f);
+			Debug.Log(hit);
+
+
+			if (hit != null && hit.collider.tag == "ground") {
+				Debug.Log(hit);
+				PlayerRG.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+				Debug.DrawLine (transform.position, hit.point, Color.cyan);
+			}
+			Debug.DrawRay(transform.position, transform.TransformDirection (Vector3.down) *1000, Color.white);
+
+			Debug.Log("jump key");			
 		}
 	}
 }
