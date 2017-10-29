dca18525c78d06430e4182bc32cc9777a68a191c
Akash Basu <33015666+FugaX@users.noreply.github.com>
2017-10-28 22:16:05 -0700

    Level Map
    Level Map which was made with Tiled(includes preview of map, tilesets, and tmx/tsx files)made with Tiled(includes preview of map, tilesets, and tmx/tsx files)

---
+++ Level Map/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="untitled " tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="platformer-tileset-template.png" width="384" height="320"/>
+</tileset>
---
+++ Level Map/untitled2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="untitled2" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="platformer-tileset-template-2.png" width="384" height="320"/>
+</tileset>
