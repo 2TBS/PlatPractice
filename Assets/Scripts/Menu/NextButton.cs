296500023a0670a178486f7e4d014e6055a42313
Akash Basu <33015666+FugaX@users.noreply.github.com>
2017-11-26 16:17:32 -0800

    Next Button  code
    A program for the "Next" button that displays the level number on the console every time the button gets clicked

---
+++ Assets/Scripts/Menu/NextButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButton : MonoBehaviour {
+    int level = 1;
+    bool clicked = true; 
+	public void Button_Click()
+    {
+        if(clicked == true) {
+
+            level++;
+            Debug.Log("Displaying level " + level);
+
+        }
+        
+    }
+}
